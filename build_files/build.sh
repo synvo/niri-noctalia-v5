527d73bea513a3b566b7289c7b917c13c649ddd8
synvo <150107299+synvo@users.noreply.github.com>
2026-08-13 20:39:09 -0400

    Add niri

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,8 @@ cp -avf "/ctx/system_files"/. /
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux
+#dnf5 install -y tmux
+dnf5 install -y niri
 dnf5 install -y noctalia
 
 # Use a COPR Example:
